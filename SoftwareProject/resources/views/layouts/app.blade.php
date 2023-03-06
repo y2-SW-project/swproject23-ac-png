08949df93a3dc15be010bd8aff6afb03c64a9698
ac-png <alicecorry@icloud.com>
2023-03-06 16:43:29 +0000

    Styling homepage and navigation bar

--- SoftwareProject/resources/views/layouts/app.blade.php
+++ SoftwareProject/resources/views/layouts/app.blade.php
@@ -20,9 +20,8 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
+                <a class="navbar-brand" href="#">DietOnline</a>
+
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -30,7 +29,17 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Products</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Diets</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Recipes</a>
+                            </li>
+                        </ul>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
@@ -56,8 +65,8 @@
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                        onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 
--- SoftwareProject/resources/views/welcome.blade.php
+++ SoftwareProject/resources/views/welcome.blade.php
@@ -1,4 +1,156 @@
 @extends('layouts.app')
 
 @section('content')
+<body>
+    <div class="container-fluid">
+        <div class="row mx-5 py-4">
+            <h2>Look What's New</h2>
+            <div class="col-md-4 col-sm-6 my-1">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">€7.99</h5>
+                        <p class="card-text">Meat-free Ham Slices</p>
+                        <a
+                            href="#"
+                            class="btn btn-primary text-light fw-semibold"
+                            >Shop All</a
+                        >
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 my-1">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">€7.99</h5>
+                        <p class="card-text">Meat-free Ham Slices</p>
+                        <a
+                            href="#"
+                            class="btn btn-primary text-light fw-semibold"
+                            >Shop All</a
+                        >
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 my-1">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">€7.99</h5>
+                        <p class="card-text">Meat-free Ham Slices</p>
+                        <a
+                            href="#"
+                            class="btn btn-primary text-light fw-semibold"
+                            >Shop All</a
+                        >
+                    </div>
+                </div>
+            </div>
+            <nav aria-label="Page navigation example">
+                <ul class="pagination justify-content-end">
+                  <li class="page-item">
+                    <a class="page-link" href="#" aria-label="Previous">
+                      <span aria-hidden="true">&laquo;</span>
+                    </a>
+                  </li>
+                  <li class="page-item">
+                    <a class="page-link" href="#" aria-label="Next">
+                      <span aria-hidden="true">&raquo;</span>
+                    </a>
+                  </li>
+                </ul>
+            </nav>
+        </div>
+        <div class="row mx-5 py-4">
+            <h2>Shop by Diet</h2>
+            <div class="col-md-3 col-sm-3 my-1">
+                <div class="card text-center mb-3">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Vegetarian</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-3 my-1">
+                <div class="card text-center mb-3">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Vegetarian</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-3 my-1">
+                <div class="card text-center mb-3">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Vegetarian</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-3 my-1">
+                <div class="card text-center mb-3">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Vegetarian</h5>
+                    </div>
+                </div>
+            </div>
+            <nav aria-label="Page navigation example">
+                <ul class="pagination justify-content-end">
+                  <li class="page-item">
+                    <a class="page-link" href="#" aria-label="Previous">
+                      <span aria-hidden="true">&laquo;</span>
+                    </a>
+                  </li>
+                  <li class="page-item">
+                    <a class="page-link" href="#" aria-label="Next">
+                      <span aria-hidden="true">&raquo;</span>
+                    </a>
+                  </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+    <footer class="bg-dark text-light p-4">
+        <div class="row justify-content-center">
+            <div class="col-8">
+                <div class="row">
+                    <div class="col-sm-3 col-lg">
+                        <p class="fs-3 fw-bold">About DietOnlie</p>
+                        <p>Store vacancies</p>
+                        <p>Careers</p>
+                        <p>DietOnline PLC</p>
+                        <p>Sustainability</p>
+                        <p>Our little helps</p>
+                    </div>
+                    <div class="col-sm-3 col-lg">
+                        <p class="fs-4 fw-bold">Here to help</p>
+                        <p>Help & FAQS</p>
+                        <p>Contact us</p>
+                    </div>
+                    <div class="col-sm-3 col-lg">
+                        <p class="fs-3 fw-bold">Our website</p>
+                        <p>Terms & Conditions</p>
+                        <p>Privacy & Cookie Policy</p>
+                        <p>Privacy Center</p>
+                        <p>Site map</p>
+                        <p>Accessibility</p>
+                    </div>
+                    <div class="col-sm-3 col-lg">
+                        <p class="fs-3 fw-bold">Useful links</p>
+                        <p>Pharmacy</p>
+                        <p>Product recall</p>
+                        <p>Store locator</p>
+                        <p>Bags of Help</p>
+                        <p>Rate this page</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
+        crossorigin="anonymous"
+    ></script>
+</body>
+
 @endsection
