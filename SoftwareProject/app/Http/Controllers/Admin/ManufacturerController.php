09a0564e30a23bc937e22db8944b8402d937ed1b
ac-png <alicecorry@icloud.com>
2023-03-20 14:05:55 +0000

    Adding appropiate file paths at the top of controllers

--- SoftwareProject/app/Http/Controllers/Admin/ManufacturerController.php
+++ SoftwareProject/app/Http/Controllers/Admin/ManufacturerController.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace App\Http\Controllers\Admin;
+
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
--- SoftwareProject/app/Http/Controllers/Admin/ProductController.php
+++ SoftwareProject/app/Http/Controllers/Admin/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
